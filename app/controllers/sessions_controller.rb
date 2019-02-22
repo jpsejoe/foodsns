50141212db9c279f7188a72672d47ded63df269a
EC2 Default User <ec2-user@ip-172-31-34-205.us-west-2.compute.internal>
2019-02-22 14:07:52 +0000

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
-      # エラーメッセージを作成する
-      flash.now[:danger] = 'Invalid email/password combination' #仮実装
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
   
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
